92a3415fb4ed983d256b43fcbb7ef633a658df43
Tristram Woolf <tristram@woolfsystems.com>
2022-12-16 19:07:07 +0000

    chore: cleanup logging

--- src/lib/child-watch-command.ts
+++ src/lib/child-watch-command.ts
@@ -1,27 +1,35 @@
+import {blue, bold, green, red} from 'cli-color'
 import {ChildProcess, spawn} from 'node:child_process'
 
 export class ChildWatchCommand {
   private app: ChildProcess | undefined
   private watchCommand: string
+  private startCount = 0
 
   constructor(watchCommand: string) {
     this.watchCommand = watchCommand
   }
 
   start(): void {
+    console.log(`[${green('-')}] starting ${bold(this.watchCommand)}`)
     this.app = this.runWatchCommand(this.watchCommand)
     this.app.on('message', (event: Event) => {
       if (event.type === 'start') {
-        console.log('nodemon started')
+        this.startCount++
+        console.log(`[${green('+')}] ${this.startCount > 1 ? 'restarted' : 'started'} ${bold(this.watchCommand)}`)
       } else if (event.type === 'crash') {
-        console.log('script crashed for some reason')
+        console.log(`[${red('!')}] crashed ${bold(this.watchCommand)}`)
+      } else {
+        console.log(`[${blue('?')}] info ${bold(this.watchCommand)}: ${JSON.stringify(event)}`)
       }
     })
   }
 
   exit(): void {
-    if (this?.app)
+    if (this?.app) {
+      console.log(`[${green('+')}] exiting: ${this.watchCommand}`)
       this.app.send('quit')
+    }
   }
 
   runWatchCommand(watchCmd: string): ChildProcess {
