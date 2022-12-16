a6adaf73dc6aa3d88c6a79dcc7e962b487a7cde2
Tristram Woolf <tristram@woolfsystems.com>
2022-12-16 19:37:07 +0000

    chore: improve logging

--- src/lib/child-watch-command.ts
+++ src/lib/child-watch-command.ts
@@ -36,7 +36,7 @@ export class ChildWatchCommand {
       }
 
       case 'log': {
-        console.log(`[${blue('?')}] (${bold(this.buildKey)}) log ${bold(this.watchCommand)}: ${JSON.stringify(event?.data?.message || event)}`)
+        console.log(`[${blue('?')}] (${bold(this.buildKey)}) log ${bold(this.watchCommand)}: ${JSON.stringify(event?.data?.colour || event)}`)
         break
       }
 
